4438904f579ed4dd5f6bf02b693a2cddf3b74263
wandreuscv <wandreuscv@unipam.edu.br>
2021-09-08 14:51:21 -0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
 export default function App(props) {
-  return <div />;
+  const [frame, setFrame] = useState();
+
+  useEffect(() => {
+    if (frame === undefined)
+      setFrame(
+        <iframe src="https://webchat.botframework.com/embed/OdontoChatBot?s=sAwl0hf_qg4.Bt73IXUFO70u7FHb1j__PDKlUPK13TGbuXRorrQgIlg" />
+      );
+  }, [frame]);
+
+  function reloadChat() {
+    setFrame(undefined);
+  }
+
+  return (
+    <main>
+      <div>
+        <button onClick={reloadChat}>Reiniciar chat</button>
+      </div>
+      {frame}
+    </main>
+  );
 }
